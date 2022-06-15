1464644175930dc0207aade38ef4b5b5ca391201
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-15 16:14:05 -0500

    create routes and controllers, install mongoose

---
+++ data/skill-data.js
@@ -0,0 +1,11 @@
+const skills = [
+  {skill: 'Javascript', proficient: false, _id: 1425523},
+  {skill: 'CSS', proficient: true, _id: 1524674 },
+  {skill: 'HTML', proficient: true, _id: 1542263},
+  {skill: 'guitar', proficient: true, _id: 1572829},
+  {skill: "GeoGuessr", proficient: true, _id: 1582747},
+]
+
+export{
+  skills
+}
